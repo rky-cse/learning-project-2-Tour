c6a9c10c7af466ee88c36d525e6fc6383d8be21e
Ravi Kumar Yadav <ravi27777777@gmail.com>
2023-12-28 15:21:30 +0530

    upload

---
+++ src/Tour.js
@@ -0,0 +1,25 @@
+import React,{useState} from 'react'
+
+
+export default function Tour({ id, image, info, name, price, removeTour }) {
+  const [readMore,setReadMore]=useState(false);
+  return (
+   <article className='single-tour'>
+    <img src={image} alt={name}></img>
+    <footer>
+      <div className='tour-info'>
+        <h4>{name}</h4>
+        <h4 className='tour-price'>${price}</h4>
+      </div>
+      <p>
+        {readMore?info:`${info.substring(0,200)}...`}
+        <button className='delete-btn' onClick={()=>{removeTour(id)}}>not interested</button>
+
+
+      </p>
+    </footer>
+
+
+   </article>
+  )
+}
